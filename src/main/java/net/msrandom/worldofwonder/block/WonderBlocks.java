5bb9ba596f4b332be89efcbe2a57f28f5128038c
MsRandom1 <ahmedtrg981106@gmail.com>
2020-05-02 19:40:40 +0300

    Sign fix

--- src/main/java/net/msrandom/worldofwonder/block/WonderBlocks.java
+++ src/main/java/net/msrandom/worldofwonder/block/WonderBlocks.java
@@ -23,7 +23,7 @@ public class WonderBlocks {
     public static final Block STEM_WOOD = add("stem_wood", new Block(Block.Properties.create(Material.WOOD, MaterialColor.WOOD).hardnessAndResistance(2.0F).sound(SoundType.WOOD)), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS));
     public static final Block STEM_LEAVES = add("stem_leaves", new LeavesBlock(Block.Properties.create(Material.LEAVES).hardnessAndResistance(0.2F).tickRandomly().sound(SoundType.PLANT).func_226896_b_()), new Item.Properties().group(ItemGroup.DECORATIONS));
     public static final Block STEM_STAIRS = add("stem_stairs", new StairsBlock(STEM_PLANKS::getDefaultState, Block.Properties.from(STEM_PLANKS)), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS));
-    public static final Block STEM_SIGN = add("stem_sign", new StandingSignBlock(Block.Properties.create(Material.WOOD).doesNotBlockMovement().hardnessAndResistance(1.0F).sound(SoundType.WOOD), WoodType.field_227038_a_), new Item.Properties().maxStackSize(16).group(ItemGroup.DECORATIONS),(block, properties) -> new SignItem(properties, block, WonderBlocks.STEM_WALL_SIGN));
+    public static final Block STEM_SIGN = add("stem_sign", new StandingSignBlock(Block.Properties.create(Material.WOOD).doesNotBlockMovement().hardnessAndResistance(1.0F).sound(SoundType.WOOD), WoodType.field_227038_a_), new Item.Properties().maxStackSize(16).group(ItemGroup.DECORATIONS), (block, properties) -> new SignItem(properties, block, WonderBlocks.STEM_WALL_SIGN));
     public static final Block STEM_DOOR = add("stem_door", new WonderDoorBlock(Block.Properties.create(Material.WOOD, STEM_PLANKS_COLOR).hardnessAndResistance(3.0F).sound(SoundType.WOOD).func_226896_b_()), new Item.Properties().group(ItemGroup.REDSTONE));
     public static final Block STEM_WALL_SIGN = add("stem_wall_sign", new WallSignBlock(Block.Properties.create(Material.WOOD).doesNotBlockMovement().hardnessAndResistance(1.0F).sound(SoundType.WOOD).lootFrom(STEM_SIGN), WoodType.field_227038_a_), null, null);
     public static final Block STEM_PRESSURE_PLATE = add("stem_pressure_plate", new WonderPressurePlateBlock(Block.Properties.create(Material.WOOD, STEM_PLANKS_COLOR).doesNotBlockMovement().hardnessAndResistance(0.5F).sound(SoundType.WOOD)), new Item.Properties().group(ItemGroup.REDSTONE));
@@ -44,10 +44,7 @@ public class WonderBlocks {
 
     private static <T extends Block> T add(String name, T block, Item.Properties properties, BiFunction<Block, Item.Properties, BlockItem> itemConstructor) {
         REGISTRY.register(name, () -> block);
-        if (itemConstructor != null) {
-            BlockItem item = itemConstructor.apply(block, properties);
-            WonderItems.REGISTRY.register(name, () -> item);
-        }
+        if (itemConstructor != null) WonderItems.REGISTRY.register(name, () -> itemConstructor.apply(block, properties));
         return block;
     }
 }
--- src/main/java/net/msrandom/worldofwonder/item/WonderItems.java
+++ src/main/java/net/msrandom/worldofwonder/item/WonderItems.java
@@ -8,4 +8,9 @@ import net.msrandom.worldofwonder.WorldOfWonder;
 public class WonderItems {
     public static final DeferredRegister<Item> REGISTRY = new DeferredRegister<>(ForgeRegistries.ITEMS, WorldOfWonder.MOD_ID);
     //public static final Item STEM_BOAT = add("stem_boat", new WonderBoatItem(new Item.Properties().maxStackSize(1).group(ItemGroup.TRANSPORTATION)));
+
+    private static <T extends Item> T add(String name, T item) {
+        REGISTRY.register(name, () -> item);
+        return item;
+    }
 }
