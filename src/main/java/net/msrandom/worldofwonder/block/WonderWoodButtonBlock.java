a931160af34d2a2eaa087842ce3302188c093c7e
MsRandom1 <ahmedtrg981106@gmail.com>
2020-05-02 19:27:06 +0300

    Wood block models

---
+++ src/main/java/net/msrandom/worldofwonder/block/WonderWoodButtonBlock.java
@@ -0,0 +1,9 @@
+package net.msrandom.worldofwonder.block;
+
+import net.minecraft.block.WoodButtonBlock;
+
+public class WonderWoodButtonBlock extends WoodButtonBlock {
+    protected WonderWoodButtonBlock(Properties properties) {
+        super(properties);
+    }
+}
