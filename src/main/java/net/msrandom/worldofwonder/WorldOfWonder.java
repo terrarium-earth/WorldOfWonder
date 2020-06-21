0d5ac53c4789fcd93638107f5b3c19206c828028
MsRandom1 <ahmedtrg981106@gmail.com>
2020-06-21 17:34:54 +0300

    Removing dist executor

--- src/main/java/net/msrandom/worldofwonder/WorldOfWonder.java
+++ src/main/java/net/msrandom/worldofwonder/WorldOfWonder.java
@@ -1,21 +1,16 @@
 package net.msrandom.worldofwonder;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.passive.AnimalEntity;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.Heightmap;
-import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.common.BiomeManager;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.msrandom.worldofwonder.block.WonderBlocks;
 import net.msrandom.worldofwonder.client.renderer.entity.DandeLionRenderer;
@@ -30,7 +25,6 @@ import net.msrandom.worldofwonder.world.gen.feature.WonderFeatures;
 @Mod(WorldOfWonder.MOD_ID)
 public class WorldOfWonder {
     public static final String MOD_ID = "worldofwonder";
-    private static final SidedExecutor SIDED_EXECUTOR;
 
     public WorldOfWonder() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -39,40 +33,18 @@ public class WorldOfWonder {
         WonderBiomes.REGISTRY.register(bus);
         WonderEntities.REGISTRY.register(bus);
         WonderFeatures.REGISTRY.register(bus);
-        bus.addListener(this::registerExtras);
+        bus.addListener(WonderBiomes::registerTypes);
+        bus.addListener(this::registerClient);
         DispenserBlock.registerDispenseBehavior(WonderItems.BLOOM_MEAL, ItemEvents.BLOOM_MEAL_DISPENSE);
         EntitySpawnPlacementRegistry.register(WonderEntities.DANDE_LION, EntitySpawnPlacementRegistry.PlacementType.ON_GROUND, Heightmap.Type.MOTION_BLOCKING, AnimalEntity::canAnimalSpawn);
     }
 
-    public void registerExtras(RegistryEvent.Register<?> event) {
-        Class<?> type = event.getRegistry().getRegistrySuperType();
-        if (type == Biome.class) {
-            BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(WonderBiomes.DANDELION_FIELDS, 5));
-            BiomeManager.addSpawnBiome(WonderBiomes.DANDELION_FIELDS);
-            BiomeDictionary.addTypes(WonderBiomes.DANDELION_FIELDS, BiomeDictionary.Type.PLAINS);
-        } else if (type == Block.class) {
-            SIDED_EXECUTOR.registerClient();
-        }
-    }
-
-    static {
-        SIDED_EXECUTOR = DistExecutor.runForDist(() -> () -> new SidedExecutor() {
-            @Override
-            protected void registerClient() {
-                super.registerClient();
-
-                //This is only called on the client, register client sided renders and stuff here
-                //RenderingRegistry.registerEntityRenderingHandler(WonderEntities.STEM_BOAT, StemBoatRenderer::new);
-                RenderingRegistry.registerEntityRenderingHandler(WonderEntities.DANDE_LION, DandeLionRenderer::new);
-                ClientRegistry.bindTileEntityRenderer(WonderTileEntities.STEM_SIGN, StemSignTileEntityRenderer::new);
-                RenderTypeLookup.setRenderLayer(WonderBlocks.STEM_DOOR, RenderType.getCutout());
-                RenderTypeLookup.setRenderLayer(WonderBlocks.STEM_TRAPDOOR, RenderType.getCutout());
-                RenderTypeLookup.setRenderLayer(WonderBlocks.STEM_LADDER, RenderType.getCutout());
-            }
-        }, () -> SidedExecutor::new);
-    }
-
-    public static class SidedExecutor {
-        protected void registerClient() {}
+    private void registerClient(FMLClientSetupEvent event) {
+        //RenderingRegistry.registerEntityRenderingHandler(WonderEntities.STEM_BOAT, StemBoatRenderer::new);
+        RenderingRegistry.registerEntityRenderingHandler(WonderEntities.DANDE_LION, DandeLionRenderer::new);
+        ClientRegistry.bindTileEntityRenderer(WonderTileEntities.STEM_SIGN, StemSignTileEntityRenderer::new);
+        RenderTypeLookup.setRenderLayer(WonderBlocks.STEM_DOOR, RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(WonderBlocks.STEM_TRAPDOOR, RenderType.getCutout());
+        RenderTypeLookup.setRenderLayer(WonderBlocks.STEM_LADDER, RenderType.getCutout());
     }
 }
