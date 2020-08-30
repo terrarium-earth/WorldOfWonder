63d5945f2795aa11be58db3be2033b84f08879d0
Ashley Wright <ahmedtrg981106@gmail.com>
2020-08-30 03:03:28 +0300

    Fix sprout placement

--- src/main/java/net/msrandom/worldofwonder/block/DandeLionSproutBlock.java
+++ src/main/java/net/msrandom/worldofwonder/block/DandeLionSproutBlock.java
@@ -14,12 +14,15 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
+import net.minecraftforge.common.IPlantable;
 import net.msrandom.worldofwonder.tileentity.WonderTileEntities;
 
 import javax.annotation.Nullable;
 
-public class DandeLionSproutBlock extends ContainerBlock {
+public class DandeLionSproutBlock extends ContainerBlock implements IPlantable {
     private static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.HORIZONTAL_AXIS;
     private static final VoxelShape SHAPE = VoxelShapes.create(0.25, 0, 0.25, 0.75, 0.5, 0.75);
 
@@ -35,6 +38,20 @@ public class DandeLionSproutBlock extends ContainerBlock {
     }
 
     @Override
+    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+        return !stateIn.isValidPosition(worldIn, currentPos) ? Blocks.AIR.getDefaultState() : super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+    }
+
+    @Override
+    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+        BlockPos blockpos = pos.down();
+        if (state.getBlock() == this) return worldIn.getBlockState(blockpos).canSustainPlant(worldIn, blockpos, Direction.UP, this);
+
+        Block block = worldIn.getBlockState(blockpos).getBlock();
+        return block == Blocks.GRASS_BLOCK || block == Blocks.DIRT || block == Blocks.COARSE_DIRT || block == Blocks.PODZOL || block == Blocks.FARMLAND;
+    }
+
+    @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
     }
@@ -42,6 +59,13 @@ public class DandeLionSproutBlock extends ContainerBlock {
     @Override
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public BlockState getPlant(IBlockReader world, BlockPos pos) {
+        BlockState state = world.getBlockState(pos);
+        if (state.getBlock() != this) return getDefaultState();
+        return state;
     }
 
     @Nullable
