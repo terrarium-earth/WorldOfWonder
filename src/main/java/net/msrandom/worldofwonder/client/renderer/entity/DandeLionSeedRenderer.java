88411fa90de9efb3af388ce9bded0a32029282df
Ashley Wright <ahmedtrg981106@gmail.com>
2021-06-24 17:34:40 +0300

    Use model part for dande-lion seed rendering

--- src/main/java/net/msrandom/worldofwonder/client/renderer/entity/DandeLionSeedRenderer.java
+++ src/main/java/net/msrandom/worldofwonder/client/renderer/entity/DandeLionSeedRenderer.java
@@ -4,48 +4,38 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.IParticleRenderType;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Matrix3f;
 import net.msrandom.worldofwonder.WorldOfWonder;
 import net.msrandom.worldofwonder.entity.DandeLionSeedEntity;
+import org.lwjgl.opengl.GL11;
 
 public class DandeLionSeedRenderer extends EntityRenderer<DandeLionSeedEntity> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(WorldOfWonder.MOD_ID, "textures/entity/dande_lion/seed.png");
+    private final ModelRenderer part = new ModelRenderer(6, 6, 0, 0);
 
     public DandeLionSeedRenderer(EntityRendererManager renderManager) {
         super(renderManager);
+        part.addBox(0, 0, 0, 6, 6, 0f);
     }
 
     @Override
     public void render(DandeLionSeedEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
-        RenderSystem.pushMatrix();
-        RenderSystem.multMatrix(matrixStackIn.last().pose());
-        RenderSystem.enableAlphaTest();
-        RenderSystem.defaultAlphaFunc();
+        matrixStackIn.pushPose();
         RenderSystem.enableDepthTest();
-        RenderSystem.enableFog();
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder buffer = tessellator.getBuilder();
-        IParticleRenderType renderType = IParticleRenderType.PARTICLE_SHEET_OPAQUE;
-        TextureManager textureManager = Minecraft.getInstance().getTextureManager();
-        renderType.begin(buffer, textureManager);
-        textureManager.bind(getTextureLocation(entityIn));
-        RenderSystem.scalef(0.16f, 0.16f, 0.16f);
-        buffer.vertex(-1, -1, 0).uv(1, 1).color(255, 255, 255, 255).uv2(packedLightIn).endVertex();
-        buffer.vertex(-1, 1, 0).uv(1, 0).color(255, 255, 255, 255).uv2(packedLightIn).endVertex();
-        buffer.vertex(1, 1, 0).uv(0, 0).color(255, 255, 255, 255).uv2(packedLightIn).endVertex();
-        buffer.vertex(1, -1, 0).uv(0, 1).color(255, 255, 255, 255).uv2(packedLightIn).endVertex();
-        renderType.end(tessellator);
-        RenderSystem.popMatrix();
         RenderSystem.depthMask(true);
-        RenderSystem.disableBlend();
-        RenderSystem.defaultAlphaFunc();
-        RenderSystem.disableFog();
+        float scale = 0.4267f;
+        matrixStackIn.scale(scale, -scale, -scale);
+        part.render(matrixStackIn, bufferIn.getBuffer(RenderType.entityCutoutNoCull(getTextureLocation(entityIn))), packedLightIn, OverlayTexture.NO_OVERLAY);
+        RenderSystem.depthMask(true);
+        matrixStackIn.popPose();
     }
 
     @Override
